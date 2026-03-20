74727c89ea1d9bb1fb7b111224718641e445462f
Myckael Morais <myckael2k@gmail.com>
2026-03-20 13:03:44 -0300

    Metodos da classe Cofrinho criados

--- src/entities/Cofrinho.java
+++ src/entities/Cofrinho.java
@@ -1,5 +1,25 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Cofrinho {
+
+    private ArrayList<Double> moeda = new ArrayList<>();
+
+    public void adicionar(double valor) {
+        moeda.add(valor);
+        System.out.println("Moeda de " + valor + " adicionada!");
+    }
+
+    public void remover(double valor){
+        moeda.remove(valor);
+        System.out.println("Moeda de " + valor + " removida!");
+    }
+
+    public void listagemMoedas(){
+        System.out.println(moeda);
+    }
+
+
 
 }
